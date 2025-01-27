bd1b4137a99aae89148a7580ca3ea0dd9fe9d6a0
Sergey Kislyakov <s.kislyakov84@gmail.com>
2025-01-27 21:35:07 +0300

    Creating a project page

--- portfolio_project/portfolio/views.py
+++ portfolio_project/portfolio/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import Project
 
 def home(request):
     return render(request, 'portfolio/home.html')
+
+def projects(request):
+    projects_list = Project.objects.all()
+    return render(request, 'portfolio/projects.html', {'projects': projects_list})
