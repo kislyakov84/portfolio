f8ffb88303069f708dae3486ddc8e817bb99d3df
Sergey Kislyakov <s.kislyakov84@gmail.com>
2025-01-28 22:46:15 +0300

    add about page

--- portfolio_project/portfolio/views.py
+++ portfolio_project/portfolio/views.py
@@ -4,6 +4,9 @@ from .models import Project
 def home(request):
     return render(request, 'portfolio/home.html')
 
+def about(request):
+    return render(request, 'portfolio/about.html')
+
 def contact(request):
     return render(request, 'portfolio/contact.html')
 
